be8f0d6938259cf93d3d4af60a2f08ab5965a4f7
lsimon <leo@lsimon.org>
2022-09-17 10:10:30 +0200

    layout: with menu

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -12,6 +12,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <div className="vertical-line" />
       <div className="horizontal-line" />
       <div className="intersection" />
+      <div className="layout--menu">Menu</div>
       <main>{children}</main>
     </div>
   );
